5e933139cc0dd632e1af42bcc6c0481ec0da0afc
Michal Pietrzak <michal.pietrzak@goodylabs.com>
2020-03-16 14:08:03 +0100

    Fixes state handling. Adds .svg icons

--- src/CalcKey.js
+++ src/CalcKey.js
@@ -3,6 +3,6 @@ import React from 'react';
 export const CalcKey = (props) => {
   const { keyvalue } = props
   return (
-      <button key={`key-${keyvalue}`} {...props}>{keyvalue}</button>
+    <button key={`key-${keyvalue}`} {...props}>{keyvalue}</button>
   )
 }
--- src/Calculator.js
+++ src/Calculator.js
@@ -11,121 +11,120 @@ const CalculateOperations = {
   '=': (prevVal, nextVal) => nextVal,
 }
 
-export const Calculator = () => {
+export class Calculator extends React.Component {
   
-  const initState = {
+  initState = {
     value: null,
     display: '0',
     currOperation: null,
     awaitOperand: false,
-    // history: []
+    fullCurrentOp: '',
+    history: []
   }
-  
-  const [state, setState] = useState(initState); 
 
-  const inputNumber = num => {
-    const { display, awaitOperand } = state
+  state = this.initState;
+  
+  inputNumber(num) {
+    const { display, awaitOperand } = this.state
     
     if (awaitOperand) {
-      setState({
-        ...state,
+      this.setState({
         display: String(num),
         awaitOperand: false
       })
     } else {
-      setState({
-        ...state,
+      this.setState({
         display: display === '0' ? String(num) : display + num
       })
     }
   }
 
-  const calculate = (nextOperation) => {
-    const { value, display, currOperation } = state;
-
+  calculate(nextOperation) {
+    const { value, display, currOperation } = this.state;
     const inputValue = parseFloat(display);
 
     if (value === null) {
-      console.log(state)
-      setState({
-        ...state,
+      this.setState({
         value: inputValue,
       })
     } else if (currOperation) {
       const currentValue = value || 0;
-      console.log(currentValue, inputValue);
       const newValue = CalculateOperations[currOperation](currentValue, inputValue);
-      setState({
-        ...state,
+      this.setState({
         value: newValue,
         display: String(newValue)
       })
     };
-    setState({
-      ...state,
+    this.setState({
       awaitOperand: true,
       currOperation: nextOperation
     })
   }
 
-  const clearAll = () => {
-    setState(initState);
+  clearAll() {
+    this.setState(this.initState);
   }
 
-  const handleKeyboardInput = e => {
+  handleKeyboardInput(e) {
     let { key } = e;
 
     if (key === 'Enter') key = '=';
 
     if ((/\d/).test(key)) {
       e.preventDefault();
-      inputNumber(parseInt(key, 10))
+      this.inputNumber(parseInt(key, 10))
     } else if (key in CalculateOperations) {
       e.preventDefault();
-      calculate(key);
+      this.calculate(key);
     } else if (key === 'c' || key === 'C') {
-      if (state.display !== '0') {
-        clearAll();
+      if (this.state.display !== '0') {
+        this.clearAll();
       }
     }
   }
 
-  useEffect(() => {
-    return window.addEventListener('keydown', handleKeyboardInput);
-  })
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyboardInput)
+  }
 
-  return (
+
+  render() {
+    return (
     
     <div className="calculator">
-      <div className="brand">Pocket calculator</div>
-      <Display value={state.display} />
-      <div className="keypad">
-        <div className="keypad__row">
-          <CalcKey className="keypad__row--key" key="calc-7" keyvalue={7} onClick={() => inputNumber(7)} />
-          <CalcKey className="keypad__row--key" key="calc-8" keyvalue={8} onClick={() => inputNumber(8)} />
-          <CalcKey className="keypad__row--key" key="calc-9" keyvalue={9} onClick={() => inputNumber(9)} />
-          <CalcKey className="keypad__row--key operation-key" key="calc-plus" onClick={() => calculate('+')} keyvalue='+' />
-        </div>
-        <div className="keypad__row">
-          <CalcKey className="keypad__row--key" key="calc-4" keyvalue={4} onClick={() => inputNumber(4)} />
-          <CalcKey className="keypad__row--key" key="calc-5" keyvalue={5} onClick={() => inputNumber(5)} />
-          <CalcKey className="keypad__row--key" key="calc-6" keyvalue={6} onClick={() => inputNumber(6)} />
-          <CalcKey className="keypad__row--key operation-key" key="calc-minus" onClick={() => calculate('-')} keyvalue='-' />
-        </div>
-        <div className="keypad__row">
-          <CalcKey className="keypad__row--key" key="calc-1" keyvalue={1} onClick={() => inputNumber(1)} />
-          <CalcKey className="keypad__row--key" key="calc-2" keyvalue={2} onClick={() => inputNumber(2)} />
-          <CalcKey className="keypad__row--key" key="calc-3" keyvalue={3} onClick={() => inputNumber(3)} />
-          <CalcKey className="keypad__row--key operation-key" key="calc-times" onClick={() => calculate('*')} keyvalue='×' />
-        </div>
-        <div className="keypad__row">
-          <CalcKey className="keypad__row--key" key="calc-clear" keyvalue='C' onClick={clearAll}/>
-          <CalcKey className="keypad__row--key" key="calc-0" keyvalue={0} onClick={() => inputNumber(0)}/>
-          <CalcKey className="keypad__row--key operation-key" key="calc-equals" onClick={() => calculate('=')} keyvalue='=' />
-          <CalcKey className="keypad__row--key operation-key" key="calc-divide" onClick={() => calculate('/')} keyvalue='÷' />
+      <div>
+        <div className="brand">Pocket calculator</div>
+        <Display value={this.state.display} />
+      </div>
+      <div>
+        <div className="keypad">
+          <div className="keypad__row">
+            <CalcKey className="keypad__row--key" key="calc-7" keyvalue={7} onClick={() => this.inputNumber(7)} />
+            <CalcKey className="keypad__row--key" key="calc-8" keyvalue={8} onClick={() => this.inputNumber(8)} />
+            <CalcKey className="keypad__row--key" key="calc-9" keyvalue={9} onClick={() => this.inputNumber(9)} />
+            <CalcKey className="keypad__row--key operation-key operation-key--plus" key="calc-plus" onClick={() => this.calculate('+')} keyvalue='' />
+          </div>
+          <div className="keypad__row">
+            <CalcKey className="keypad__row--key" key="calc-4" keyvalue={4} onClick={() => this.inputNumber(4)} />
+            <CalcKey className="keypad__row--key" key="calc-5" keyvalue={5} onClick={() => this.inputNumber(5)} />
+            <CalcKey className="keypad__row--key" key="calc-6" keyvalue={6} onClick={() => this.inputNumber(6)} />
+            <CalcKey className="keypad__row--key operation-key operation-key--minus" key="calc-minus" onClick={() => this.calculate('-')} keyvalue='' />
+          </div>
+          <div className="keypad__row">
+            <CalcKey className="keypad__row--key" key="calc-1" keyvalue={1} onClick={() => this.inputNumber(1)} />
+            <CalcKey className="keypad__row--key" key="calc-2" keyvalue={2} onClick={() => this.inputNumber(2)} />
+            <CalcKey className="keypad__row--key" key="calc-3" keyvalue={3} onClick={() => this.inputNumber(3)} />
+            <CalcKey className="keypad__row--key operation-key operation-key--multiply" key="calc-times" onClick={() => this.calculate('*')} keyvalue='' />
+          </div>
+          <div className="keypad__row">
+            <CalcKey className="keypad__row--key" key="calc-clear" keyvalue='C' onClick={() => this.clearAll()}/>
+            <CalcKey className="keypad__row--key" key="calc-0" keyvalue={0} onClick={() => this.inputNumber(0)}/>
+            <CalcKey className="keypad__row--key operation-key operation-key--equals" key="calc-equals" onClick={() => this.calculate('=')} keyvalue='' />
+            <CalcKey className="keypad__row--key operation-key operation-key--divide" key="calc-divide" onClick={() => this.calculate('/')} keyvalue='' />
+          </div>
         </div>
       </div>
     </div>
-  )
+  )}
 }
 
